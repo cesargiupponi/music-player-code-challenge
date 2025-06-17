4091be7798915e12f87d0d47532c45cb384c8c8c
Cesar Giupponi <cesar.giupponi@gmail.com>
2025-06-16 22:26:34 -0300

    feat: setup the project

---
+++ MusicPlayer/MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Cesar Giupponi on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
