aa4957799afa31514bbaaa044b12ecad6bfe5c16
Cesar Giupponi <cesar.giupponi@gmail.com>
2025-06-17 22:53:09 -0300

    fix: set navigation bar, search bar and search text field  appearance

--- MusicPlayer/MusicPlayer/MusicPlayerApp.swift
+++ MusicPlayer/MusicPlayer/MusicPlayerApp.swift
@@ -9,6 +9,22 @@ import SwiftUI
 
 @main
 struct MusicPlayerApp: App {
+
+    let navBarAppearence = UINavigationBarAppearance()
+
+    init() {
+        navBarAppearence.configureWithOpaqueBackground()
+        navBarAppearence.backgroundColor = .black
+        navBarAppearence.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearence.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+        UINavigationBar.appearance().standardAppearance = navBarAppearence
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearence
+        UISearchTextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).defaultTextAttributes = [.foregroundColor: UIColor.white]
+        UISearchTextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).tintColor = .white
+        UISearchBar.appearance().tintColor = .white
+    }
+
     var body: some Scene {
         WindowGroup {
             SongsView()
